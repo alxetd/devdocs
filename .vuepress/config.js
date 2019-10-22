8c9f64cedd7f988957f40c99b0068a54dafccab7
Alexandru Trandafir <eduard.trandafir@mrm-mccann.com>
2019-10-22 16:02:33 +0300

    Add additional structure

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,13 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'VueJs', link: '/vuejs/' },
       { text: 'Laravel', link: '/laravel/' },
-      { text: 'Drupal', link: '/drupal/' }
+      { text: 'Php', link: '/php/' },
+      { text: 'Javascript', link: '/javascript/' },
+      { text: 'React', link: '/react/' },
+      { text: 'Css', link: '/css/' },
+      { text: 'Html', link: '/html/' },
+      { text: 'Design Patterns', link: '/design-patterns/' },
+      { text: 'Interview questions', link: '/interview-questions/' }
     ],
     sidebar: [
       '/',
---
+++ React/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+footer: MIT Licensed
+---
+# React
